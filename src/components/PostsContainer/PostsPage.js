40a830c3c0c8e3091b4c990c60f8e6f5d85cbaff
Kyle Lovell <ky2vell@gmail.com>
2020-04-26 08:45:52 -0400

    like icon onClick event added

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,15 @@
-//Complete the necessary code in this file
-// import useState
-import React from "react";
-import Post from "./Post";
-import "./Posts.css";
-// import data 
+import React, { useState } from 'react';
+import Post from './Post';
+import './Posts.css';
+import dummyData from '../../dummy-data';
 
 const PostsPage = () => {
-  // set up state for your data
+  const [posts, setPosts] = useState(dummyData);
   return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+    <div className='posts-container-wrapper'>
+      {posts.map((post, index) => (
+        <Post key={index} post={post} />
+      ))}
     </div>
   );
 };
